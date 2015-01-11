022b2194aff96f6a43bab22a02eda03b6da59783
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2015-01-11 14:28:08 -0800

    libunwind: Add missing include of libunwind_ext.h to CompactUnwinder.hpp, DwarfInstructions.hpp, and UnwindCursor.hpp
    http://www.llvm.org/bugs/show_bug.cgi?id=22202
    
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- src/UnwindCursor.hpp
+++ src/UnwindCursor.hpp
@@ -30,6 +30,7 @@
 #include "DwarfInstructions.hpp"
 #include "EHHeaderParser.hpp"
 #include "libunwind.h"
+#include "libunwind_ext.h"
 #include "Registers.hpp"
 #include "RWMutex.hpp"
 #include "Unwind-EHABI.h"
